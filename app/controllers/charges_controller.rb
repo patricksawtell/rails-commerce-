73118bef7a91f41379449bef28d20ae41be3929a
patricksawtell <patrick.sawtell@gmail.com>
2017-03-27 14:07:10 -0700

    created working purchases migration and controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,19 +1,22 @@
 class ChargesController < ApplicationController 
 
 
-def create
+	def create
 	
-	customer = Stripe::Customer.create(
-		:email => params[:stripeEmail],
-		:source  => params[:stripeToken]
+		customer = Stripe::Customer.create(
+			:email => params[:stripeEmail],
+			:source  => params[:stripeToken]
 		)
 
-	charge = Stripe::Charge.create(
-		:customer    => customer.id,
-		:amount      => params[:amount],
-		:description => 'Rails Stripe customer',
-		:currency    => 'usd'
+		charge = Stripe::Charge.create(
+			:customer    => customer.id,
+			:amount      => params[:amount],
+			:description => 'Rails Stripe customer',
+			:currency    => 'usd'
 		)
+
+		purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], amount: params[:amount], description: charge.description, currency: charge.currency, customer_id: customer.id, product_id:1)
+		redirect_to purchase
 
  	rescue Stripe::CardError => e
  	  flash[:error] = e.message
