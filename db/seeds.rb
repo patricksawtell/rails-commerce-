5999a1ac77152ecd4a64e4cf7fae86ca62b0cf68
patricksawtell <patrick.sawtell@gmail.com>
2017-03-28 15:01:48 -0700

    seed data and added  products table

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,8 @@
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
-demo = Product.create(title: "Dream Big", subtitle: "lorum ipsum" image_name: "dreambig.png", price: "5.99", sku: "dreamBig", download_url: "https://s3.ca-central-1.amazonaws.com/railscommerce/dreambig.png", details: "Give a man a program, frustrate him for a day. Teach a man to program, frustrate him for a lifetime", description: %{<p>It's your purchase</p>})
+dreamBig = Product.create(title: "Dream Big", subtitle: "lorum ipsum" image_name: "dreambig.png", price: "5.99", sku: "dreamBig", download_url: "https://s3.ca-central-1.amazonaws.com/railscommerce/dreambig.png", details: "Give a man a program, frustrate him for a day. Teach a man to program, frustrate him for a lifetime", description: %{<p>It's your purchase</p>})
+
+bestCat = Product.create(title: "Mr.Cat", subtitle: "lorum ipsum" image_name: "bestcatever.png", price: "5.99", sku: "bestCat", download_url: "https://s3.ca-central-1.amazonaws.com/railscommerce/bestcatever.png", details: "The Best Cat.", description: %{<p>It's the best cat ever!</p>})
+
+	
